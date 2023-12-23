b19b54b6aefbe89f53b3fe7c5d1c08423e2e8245
a-abdirahim <cabtamido55@gmail.com>
2023-12-23 17:34:13 +0300

    first commit

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const DB = process.env.DB;
 const app = require("./app");
 
 // mongoose.connect(DB).then(() => console.log("DB connected successfully"));
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-  })
-  .then(() => console.log("DB connection successful!"));
+mongoose.connect(DB).then(() => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
